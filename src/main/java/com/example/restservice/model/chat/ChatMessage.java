f8875e001c67af2d1fc64508b6e93cc27e038d5f
michael78r <michaelrakotonanahary78@gmail.com>
2026-04-22 23:55:53 +0400

    add model - chat

---
+++ src/main/java/com/example/restservice/model/chat/ChatMessage.java
@@ -0,0 +1,46 @@
+package com.example.restservice.controller.chat;
+
+public class ChatMessage {
+    private String sender;
+    private String content;
+    private MessageType type;
+
+    public enum MessageType {
+        CHAT,
+        JOIN,
+        LEAVE
+    }
+
+    public ChatMessage() {
+    }
+
+    public ChatMessage(String sender, String content, MessageType type) {
+        this.sender = sender;
+        this.content = content;
+        this.type = type;
+    }
+
+    public String getSender() {
+        return sender;
+    }
+
+    public void setSender(String sender) {
+        this.sender = sender;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public MessageType getType() {
+        return type;
+    }
+
+    public void setType(MessageType type) {
+        this.type = type;
+    }
+}
