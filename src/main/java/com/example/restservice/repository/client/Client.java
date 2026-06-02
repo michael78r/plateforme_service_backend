0bd824147f3dfe13929461cb469916b3fb717977
michael78r <michaelrakotonanahary78@gmail.com>
2026-06-02 19:13:31 +0400

    update client repo

---
+++ src/main/java/com/example/restservice/repository/client/Client.java
@@ -0,0 +1,12 @@
+package com.example.restservice.repository.client;
+
+public class Client extends com.example.restservice.model.client.Client {
+    public Client() {
+        super();
+    }
+
+    public Client(String name, String email) {
+        super(name, email);
+    }
+    
+}
