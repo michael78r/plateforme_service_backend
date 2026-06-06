651299200d0dedf35296c6d20bfc39b6ec1e519b
michael78r <michaelrakotonanahary78@gmail.com>
2026-06-06 20:27:00 +0400

    fixed orderRepo

--- src/main/java/com/example/restservice/repository/order/OrderRepository.java
+++ src/main/java/com/example/restservice/repository/order/OrderRepository.java
@@ -4,6 +4,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.example.restservice.model.order.Order;
 
-public class OrderRepository extends JpaRepository<Order, Long> {
+public interface OrderRepository extends JpaRepository<Order, Long> {
     
 }
