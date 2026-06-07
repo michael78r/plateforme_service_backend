7116b5810e1461041cae5510c529987cac7b2c88
michael78r <michaelrakotonanahary78@gmail.com>
2026-06-07 05:03:47 +0400

    update repo order

--- src/main/java/com/example/restservice/order/OrderRepository.java
+++ src/main/java/com/example/restservice/order/OrderRepository.java
@@ -1,7 +1,11 @@
 package com.example.restservice.order;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface OrderRepository extends JpaRepository<Order, Long> {
-
+    List<Order> findByClientIdOrderByCreatedAtDesc(Long clientId);
 }
