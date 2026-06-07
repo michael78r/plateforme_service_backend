a4c4e5dc69fd810243a8c4848cd6defeaeb5183d
michael78r <michaelrakotonanahary78@gmail.com>
2026-06-07 04:48:26 +0400

    updated repo invoice

--- src/main/java/com/example/restservice/invoice/InvoiceRepository.java
+++ src/main/java/com/example/restservice/invoice/InvoiceRepository.java
@@ -1,9 +1,12 @@
 package com.example.restservice.invoice;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface InvoiceRepository extends JpaRepository<Invoice, Long> {
-
+    Optional<Invoice> findByOrderId(Long orderId);
+    Optional<Invoice> findByNumber(String number);
 }
