de8df24517fe9579aa1d5fb7461d4cba623932dd
michael78r <michaelrakotonanahary78@gmail.com>
2026-06-08 07:06:17 +0400

    remove cors dans authcontroller

--- src/main/java/com/example/restservice/user/AuthController.java
+++ src/main/java/com/example/restservice/user/AuthController.java
@@ -3,7 +3,6 @@ package com.example.restservice.user;
 import java.util.Map;
 
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -12,7 +11,6 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/auth")
-@CrossOrigin(origins = "http://localhost:4200")
 public class AuthController {
 
     private final AuthService authService;
