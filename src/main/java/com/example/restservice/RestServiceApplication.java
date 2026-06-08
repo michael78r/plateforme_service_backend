eb209f1f56e8d5a4a11fc19a2f60fba17a4188c6
michael78r <michaelrakotonanahary78@gmail.com>
2026-06-08 18:59:38 +0400

    enable async scheduling - starting notification email

--- src/main/java/com/example/restservice/RestServiceApplication.java
+++ src/main/java/com/example/restservice/RestServiceApplication.java
@@ -3,12 +3,14 @@ package com.example.restservice;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.scheduling.annotation.EnableAsync;
 
 import com.example.restservice.shared.security.AppJwtProperties;
 
 
 @SpringBootApplication
 @EnableConfigurationProperties(AppJwtProperties.class)
+@EnableAsync
 
 public class RestServiceApplication {
 
